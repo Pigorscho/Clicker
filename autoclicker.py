6d6fb7020eef0ec6931306255ef4c76bfa01434d
Pigorscho <lennartpigorsch@gmail.com>
2022-06-27 16:44:01 +0200

    finished ui structure of autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -6,104 +6,117 @@ class Example():
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("AutoClicker GUI")
-        self.root.wm_geometry('950x500')
-        self.root.resizable(width=False, height=False)
+        self.root.wm_geometry('950x400')
+        #self.root.resizable(width=False, height=False)
 
         # click interval widget
-        self.click_interval = tk.Frame(self.root, bg="red", height=75)
-        hours_input = tk.Entry(self.click_interval, width=5)
-        mins_input = tk.Entry(self.click_interval, width=5)
-        secs_input = tk.Entry(self.click_interval, width=5)
-        millisecs_input = tk.Entry(self.click_interval, width=5)
+        self.click_interval = tk.Frame(self.root, bg="red", height=75, padx=10, pady=10)
+        self.click_interval_frame = tk.Frame(self.click_interval, bg="blue violet", height=75, padx=10, pady=10)
+        hours_input = tk.Entry(self.click_interval_frame, width=5,)
+        mins_input = tk.Entry(self.click_interval_frame, width=5)
+        secs_input = tk.Entry(self.click_interval_frame, width=5)
+        millisecs_input = tk.Entry(self.click_interval_frame, width=5)
 
-        click_intervall_label = tk.Label(self.click_interval, text='Click Intervall :')
+        click_intervall_label = tk.Label(self.click_interval_frame, text='Click Intervall :')
 
-        hours_label = tk.Label(self.click_interval, text='hours')
-        mins_label = tk.Label(self.click_interval, text='mins')
-        secs_label = tk.Label(self.click_interval, text='secs')
-        millisecs_label = tk.Label(self.click_interval, text='milliseconds')
+        hours_label = tk.Label(self.click_interval_frame, text='hours', padx=30)
+        mins_label = tk.Label(self.click_interval_frame, text='mins', padx=30)
+        secs_label = tk.Label(self.click_interval_frame, text='secs', padx=30)
+        millisecs_label = tk.Label(self.click_interval_frame, text='milliseconds', padx=30)
 
-        click_intervall_label.grid(row=1, column=1)
-        hours_input.grid(row=2, column=1)
-        hours_label.grid(row=2, column=2)
-        mins_input.grid(row=2, column=3)
-        mins_label.grid(row=2, column=4)
-        secs_input.grid(row=2, column=5)
-        secs_label.grid(row=2, column=6)
-        millisecs_input.grid(row=2, column=7)
-        millisecs_label.grid(row=2, column=8)
+        click_intervall_label.pack()
+        hours_input.pack(side='left', expand=True)
+        hours_label.pack(side='left', expand=True)
+        mins_input.pack(side='left', expand=True)
+        mins_label.pack(side='left', expand=True)
+        secs_input.pack(side='left', expand=True)
+        secs_label.pack(side='left', expand=True)
+        millisecs_input.pack(side='left', expand=True)
+        millisecs_label.pack(side='left', expand=True)
 
         self.click_interval.pack(fill='both', expand=True)
+        self.click_interval_frame.pack()
 
 
 
         # center click options
-        self.center_click_options = tk.Frame(self.root, width=150, height=75, bg="cyan")
+        self.center_click_options = tk.Frame(self.root, width=150, height=75, bg="lightblue", padx=10, pady=10)
         ## click options
-        self.click_options = tk.Frame(self.center_click_options, width=150, height=75, bg='yellow')
+        self.click_options = tk.Frame(self.center_click_options, width=150, height=75, bg='yellow', padx=10, pady=10)
         ## click repeat
-        self.click_repeat = tk.Frame(self.center_click_options, width=150, bg='green')
-        ### click options content
-        click_options_header_label = tk.Label(self.click_options, text='Click Options :')
-        click_options_header_label.grid(row=0, column=1)
+        self.click_repeat = tk.Frame(self.center_click_options, width=150, bg='green', padx=10, pady=10)
+        ### click options content (click_options & click_type)
+        self.click_options_frame = tk.Frame(self.click_options, width=150, height=75, bg='cyan', padx=10, pady=10)
+        click_options_header_label = tk.Label(self.click_options_frame, text='Click Options :')
+        click_options_header_label.grid(row=0, column=0)
 
-        mouse_button_label = tk.Label(self.click_options, text='Mouse Button :')
-        mouse_button_label.grid(row=2, column=1)
+        mouse_button_label = tk.Label(self.click_options_frame, text='Mouse Button :')
+        mouse_button_label.grid(row=1, column=0)
 
-        mouse_button_combo_box = ttk.Combobox(self.click_options, values=['Left',
+        mouse_button_combo_box = ttk.Combobox(self.click_options_frame, values=['Left',
                                                                           'Right',
                                                                           'Mousewheel'])
-        mouse_button_combo_box.grid(row=2, column=2)
+        mouse_button_combo_box.grid(row=1, column=1)
         mouse_button_combo_box.current(0)
 # missing : comboExample.bind("<<ComboboxSelected>>", callbackFunc)
-        click_type_header_label = tk.Label(self.click_options, text='Click Type :')
-        click_type_header_label.grid(row=3, column=1)
+        click_type_header_label = tk.Label(self.click_options_frame, text='Click Type :')
+        click_type_header_label.grid(row=2, column=0)
 
-        click_type_combo_box = ttk.Combobox(self.click_options, values=['Single',
+        click_type_combo_box = ttk.Combobox(self.click_options_frame, values=['Single',
                                                                         'Double'])
-        click_type_combo_box.grid(row=4, column=2)
+        click_type_combo_box.grid(row=2, column=1)
         click_type_combo_box.current(0)
 # missing : comboExample.bind("<<ComboboxSelected>>", callbackFunc)
 
-        click_type_label = tk.Label(self.click_options, text='Mouse Button :')
-        click_type_label.grid(row=4, column=1)
+
 
         self.click_options.pack(side="left", fill="both", expand=True)
         self.click_repeat.pack(side="right", fill="both", expand=True)
+        self.click_options_frame.pack()
+        ## click repeat window
+        self.click_repeat_frame_all = tk.Frame(self.click_repeat, width=150, height=75, bg='gold', padx=10, pady=10)
         ## click repeat content
-        click_repeat_header_label = tk.Label(self.click_repeat, text='Click Repeat :')
-        click_repeat_header_label.grid(row=0, column=1)
 
-        click_repeat_button = tk.Radiobutton(self.click_repeat, text='Repeat :', value=1)  # , variable=makro_button_on
-        click_repeat_button.grid(row=1, column=1)
+        self.click_repeat_frame = tk.Frame(self.click_repeat_frame_all, width=150, height=75, bg='purple', padx=10, pady=10)
+        click_repeat_header_label = tk.Label(self.click_repeat_frame, text='Click Repeat :')
+        click_repeat_header_label.grid(row=0, column=0)
 
-        click_repeat_combo_box = ttk.Combobox(self.click_repeat, values=['1', '2', '3', '4', '5'])
-        click_repeat_combo_box.grid(row=1, column=2)
+        click_repeat_button = tk.Radiobutton(self.click_repeat_frame, text='Repeat :', value=1)  # , variable=makro_button_on
+        click_repeat_button.grid(row=1, column=0)
+
+        click_repeat_combo_box = ttk.Combobox(self.click_repeat_frame, values=['1', '2', '3', '4', '5'], width=5)
+        click_repeat_combo_box.grid(row=1, column=1)
         click_repeat_combo_box.current(0)
 
-        click_repeat_combo_box_label = tk.Label(self.click_repeat, text='times')
-        click_repeat_combo_box_label.grid(row=2, column=2)
+        click_repeat_combo_box_label = tk.Label(self.click_repeat_frame, text='times')
+        click_repeat_combo_box_label.grid(row=1, column=2)
 
-        repeat_until_stopped_button = tk.Radiobutton(self.click_repeat, text='Repeat until stopped', value=1)  # , variable=makro_button_on
-        repeat_until_stopped_button.grid(row=3, column=1)
+        # repeat_until_stopped_frame
+        self.repeat_until_stopped_frame = tk.Frame(self.click_repeat_frame_all, width=250, height=75, bg='limegreen', padx=10, pady=10)
+        repeat_until_stopped_button = tk.Radiobutton(self.repeat_until_stopped_frame, text='Repeat until stopped', value=1)  # , variable=makro_button_on
+        repeat_until_stopped_button.grid(row=0, column=0)
+
+        self.click_repeat_frame_all.pack()
+        self.click_repeat_frame.pack()
+        self.repeat_until_stopped_frame.pack()
 
 
 
         # makro
-        self.makro = tk.Frame(self.root, width=200, height=250)
+        self.makro = tk.Frame(self.root, width=200, height=250, bg='brown', padx=10, pady=10)
         # makro frame 1
-        self.makro_settings_frame = tk.Frame(self.makro, width=100, height=350, bg='blue')
+        self.makro_settings_frame = tk.Frame(self.makro, width=100, height=350, bg='blue', padx=10, pady=10)
         makro_radio_button = tk.Radiobutton(self.makro_settings_frame, text='Makro', value=1) # , variable=makro_button_on
         makro_radio_button.grid(row=0, column=0)
         # makro button aufnehmen
         record_makro = tk.Button(self.makro_settings_frame, text='REC')
         record_makro.grid(row=0, column=1)
         makro_combo_box = ttk.Combobox(self.makro_settings_frame, values=['Makro 1', 'Makro 2', 'Makro 3', 'Makro 4', 'Makro 5'])
-        makro_combo_box.grid(row=1)
+        makro_combo_box.grid(row=1, columnspan=2)
         makro_combo_box.current(0)
 # missing : comboExample.bind("<<ComboboxSelected>>", callbackFunc)
         # makro frame 2
-        self.makro_text_frame = tk.Frame(self.makro, width=100, height=350, bg='white')
+        self.makro_text_frame = tk.Frame(self.makro, width=100, height=350, bg='white', padx=10, pady=10)
         makro_rec_text_label = tk.Label(self.makro_text_frame, text='makros')
         makro_rec_text_label.pack()
 
@@ -112,15 +125,36 @@ class Example():
 
 
         # special eff
-        self.special_effects = tk.Frame(self.root, width=150, height=60, bg='orange')
+        self.special_effects = tk.Frame(self.root, width=150, height=60, bg='orange', padx=10, pady=10)
+        self.special_effects_frame = tk.Frame(self.special_effects, width=150, height=60, bg='white', padx=10, pady=10)
 
-        special_effect_button = tk.Radiobutton(self.special_effects, text='Cheatcode :', value=1)  # , variable=makro_button_on
-        special_effect_button.grid(row=0, column=0)
+        special_effect_button = tk.Radiobutton(self.special_effects_frame, text='Cheatcode :', value=1)  # , variable=makro_button_on
+        special_effect_button.grid(column=0, row=0)
 
-        special_effect_combo_box = ttk.Combobox(self.special_effects,
+        special_effect_combo_box = ttk.Combobox(self.special_effects_frame,
                                        values=['Cheat 1', 'Cheat 2', 'Cheat 3', 'Cheat 4', 'Cheat 5'])
-        special_effect_combo_box.grid(row=0, column=1)
+        special_effect_combo_box.grid(column=1, row=0)
         special_effect_combo_box.current(0)
+
+        self.special_effects_frame.pack()
+
+        # handle button frame
+        self.handle_button = tk.Frame(self.root, width=150, height=200, bg='lightgray', padx=10, pady=10)
+        self.handle_button_frame = tk.Frame(self.handle_button, width=150, height=200, bg='black', padx=10, pady=10)
+
+        start_button = tk.Button(self.handle_button_frame, text='START')
+        stop_button = tk.Button(self.handle_button_frame, text='STOP')
+        hotkey_settings_button = tk.Button(self.handle_button_frame, text='Hotkey Setting')
+
+        start_button.grid(column=1, row=0)
+        stop_button.grid(column=2, row=0)
+        hotkey_settings_button.grid(column=3, row=0)
+
+        self.handle_button_frame
+
+
+
+        self.handle_button_frame.pack()
 
 
 
@@ -128,6 +162,7 @@ class Example():
         self.center_click_options.grid(row=1, column=1, rowspan=2, sticky='nsew')
         self.makro.grid(row=0, column=2, rowspan=3, sticky='nsew')
         self.special_effects.grid(row=3, columnspan=3, rowspan=1, sticky='ew')
+        self.handle_button.grid(row=4, columnspan=3, rowspan=1, sticky='nsew')
 
 
 
